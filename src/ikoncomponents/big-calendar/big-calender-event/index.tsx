d46afd485e1619305c1b7c18df550b0e3cf1cdf9
ankit-paul-keross <ankit.paul@keross.com>
2025-11-03 12:58:15 +0530

    added big calendar

---
+++ src/ikoncomponents/big-calendar/big-calender-event/index.tsx
@@ -0,0 +1,38 @@
+import { Eye, SquarePen } from "lucide-react";
+import { BigCalendarEventProps, ExtraParamsEvent } from "../type";
+import { TooltipComponent as Tooltip } from "../../tooltip";
+
+// Custom event component
+export default function BigCalenderEvent({ event, extraParamsEvent }: { event: BigCalendarEventProps, extraParamsEvent?: ExtraParamsEvent }) {
+    return (
+        <div className="custom-event flex flex-row justify-between">
+            <span className="truncate">{event.title}</span>
+            <span className="flex flex-row gap-1">
+                {(extraParamsEvent?.isEditableAll || event.isEditable) &&
+                    <Tooltip tooltipContent="Edit">
+                        <button
+                            className="event-edit-button"
+                            onClick={(e) => {
+                                e.stopPropagation(); // Prevent triggering other event handlers
+                                extraParamsEvent?.onEditEventClick?.(event);
+                            }}
+                        >
+                            <SquarePen size={16} />
+                        </button>
+                    </Tooltip>
+                }
+                <Tooltip tooltipContent="View">
+                    <button
+                        className="event-view-button"
+                        onClick={(e) => {
+                            e.stopPropagation(); // Prevent triggering other event handlers
+                            extraParamsEvent?.onViewEventClick?.(event);
+                        }}
+                    >
+                        <Eye size={16} />
+                    </button>
+                </Tooltip>
+            </span>
+        </div>
+    );
+}
---
+++ src/ikoncomponents/big-calendar/type.ts
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+//import { Event } from "react-big-calendar";
+
+export interface BigCalendarProps {
+  events: BigCalendarEventProps[];
+  extraParamsEvent?: ExtraParamsEvent;
+  extraTools?: ReactNode[];
+  onDateClick?: (date: Date) => void;
+}
+
+export interface ExtraParamsEvent {
+  defaultView?: string;
+  isEditableAll?: boolean;
+  isViewable?: boolean;
+  onEditEventClick?: (event: BigCalendarEventProps) => void;
+  onViewEventClick?: (event: BigCalendarEventProps) => void;
+  height?: string;
+  margin?: string;
+}
+
+export interface BigCalendarEventProps extends Event {
+  title?: string
+  isEditable?: boolean;
+  isViewable?: boolean;
+  start: Date;
+  end: Date;
+  onEventClick?: (event: any) => void;
+}
+
+export interface BigCalenderToolbarProps {
+  onNavigate: (view: any) => void;
+  onView: (view: any) => void;
+  label: string;
+  extraTools?: ReactNode[];
+  view: "month" | "week" | "work_week" | "day" | "agenda";
+  //extraParamsEvent?: ExtraParamsEvent;
+}
