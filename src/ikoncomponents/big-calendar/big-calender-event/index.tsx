d51ee377b67a6694956707fc7745fe8a718d3ca9
ankit-paul-keross <ankit.paul@keross.com>
2025-11-26 06:44:21 +0000

    edit big calendar event styles

--- src/ikoncomponents/big-calendar/big-calender-event/index.tsx
+++ src/ikoncomponents/big-calendar/big-calender-event/index.tsx
@@ -6,12 +6,12 @@ import { TooltipComponent as Tooltip } from "../../tooltip";
 export default function BigCalenderEvent({ event, extraParamsEvent }: { event: BigCalendarEventProps, extraParamsEvent?: ExtraParamsEvent }) {
     return (
         <div className="custom-event flex flex-row justify-between">
-            <span className="truncate">{event.title}</span>
+            <span className="truncate w-auto">{event.title}</span>
             <span className="flex flex-row gap-1">
                 {(extraParamsEvent?.isEditableAll || event.isEditable) &&
                     <Tooltip tooltipContent="Edit">
                         <button
-                            className="event-edit-button"
+                            className="event-edit-button w-fit px-1"
                             onClick={(e) => {
                                 e.stopPropagation(); // Prevent triggering other event handlers
                                 extraParamsEvent?.onEditEventClick?.(event);
@@ -23,7 +23,7 @@ export default function BigCalenderEvent({ event, extraParamsEvent }: { event: B
                 }
                 <Tooltip tooltipContent="View">
                     <button
-                        className="event-view-button"
+                        className="event-view-button w-fit px-1"
                         onClick={(e) => {
                             e.stopPropagation(); // Prevent triggering other event handlers
                             extraParamsEvent?.onViewEventClick?.(event);
