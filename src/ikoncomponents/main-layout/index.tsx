c24b757e9f6ead002cf57f818c13c99aa656cbb6
Abhishek Show <abhishek.show@keross.com>
2026-01-15 15:25:50 +0530

    Provider wrapper changes

--- src/ikoncomponents/main-layout/index.tsx
+++ src/ikoncomponents/main-layout/index.tsx
@@ -18,6 +18,7 @@ export function MainLayout({
 }) {
     return (
         <>
+        
             <MainSidebar baseUrl={baseUrl} platformUrl={platformUrl} />
             <SidebarProvider>
                 <DialogProvider>
--- src/ikoncomponents/provider-wrapper/index.tsx
+++ src/ikoncomponents/provider-wrapper/index.tsx
@@ -6,6 +6,7 @@ import { FontProvider } from "../../utils/font-provider";
 import { RadiusProvider } from "../../utils/border-radius-provider";
 import { BreadcrumbProvider } from "../app-breadcrumb/BreadcrumbProvider";
 import { MainLayout } from "../main-layout";
+import { RefreshProvider } from "../main-layout/RefreshContext";
 
 export function ProviderWrapper({ 
     children, 
@@ -21,9 +22,11 @@ export function ProviderWrapper({
             <FontProvider>
                 <RadiusProvider>
                     <BreadcrumbProvider>
+                    <RefreshProvider>
                         <MainLayout baseUrl={baseUrl} platformUrl={platformUrl}>
                             {children}
                         </MainLayout>
+                    </RefreshProvider>
                     </BreadcrumbProvider>
                 </RadiusProvider>
             </FontProvider>
